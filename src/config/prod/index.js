369a87aeb3fd97ca0188bdba291f498ff372fbfc
Karim Vaes <karim@kvaes.be>
2018-05-03 15:11:16 +0200

    Configs for public sites

--- src/config/prod/index.js
+++ src/config/prod/index.js
@@ -1,6 +1,8 @@
 export default {
   serverURI: '',
   apiGetVmSize: 'https://vmchooser.azure-api.net/v2/api/GetVmSize',
+  apiGetVmDetail: 'https://vmchooser.azure-api.net/v2/api/GetVmDetail',
+  apiCalcVmOptimizations: 'https://vmchooser.azure-api.net/v2/api/CalcVmOptimizations',
   apiGetDiskConfig: 'https://vmchooser.azure-api.net/v2/api/GetDiskConfig',
   fixedLayout: false,
   hideLogoOnMobile: false
