69b91f2d4262df3d031735c68c4b647bae934933
Karim Vaes <karim@kvaes.be>
2018-05-25 23:46:52 +0200

    Changed region select to dynamic population

--- src/components/views/VmOptimizer.vue
+++ src/components/views/VmOptimizer.vue
@@ -36,42 +36,10 @@
           <br />
           <div class="input-group">
             <select class="form-control" v-model="region">
-              <option disabled value="">Deployment region</option>
-              <option value="asia-pacific-east">Asia Pacific East</option>
-              <option value="asia-pacific-southeast">Asia Pacific South-East</option>
-              <option value="australia-east">Australia East</option>
-              <option value="australia-southeast">Australia South East</option>
-              <option value="brazil-south">Brazil South</option>
-              <option value="canada-central">Canada Central</option>
-              <option value="canada-east">Canada East</option>
-              <option value="central-india">India Central</option>
-              <option value="west-india">India West</option>
-              <option value="south-india">India South</option>
-              <option value="europe-north">Europe North</option>
-              <option value="europe-west" selected="selected">Europe West</option>
-              <option value="france-central">France Central</option>
-              <option value="france-south">France South</option>
-              <option value="germany-central">Germany Central</option>
-              <option value="germany-northeast">Germany North East</option>
-              <option value="japan-east">Japan East</option>
-              <option value="japan-west">Japan West</option>
-              <option value="korea-central">Korea Central</option>
-              <option value="korea-south">Korea South</option>
-              <option value="united-kingdom-south">UK South</option>
-              <option value="united-kingdom-west">UK West</option>
-              <option value="us-central">US Central</option>
-              <option value="us-east">US East</option>
-              <option value="us-east-2">US East 2</option>
-              <option value="usgov-arizona">US Gov Arizona</option>
-              <option value="usgov-iowa">US Gov Iowa</option>
-              <option value="usgov-texas">US Gov Texas</option>
-              <option value="usgov-virginia">US Gov Virginia</option>
-              <option value="us-north-central">US North Central</option>
-              <option value="us-south-central">US South Central</option>
-              <option value="us-west">US West</option>
-              <option value="us-west-2">US West 2</option>
-              <option value="us-west-central">US West Central</option>
-              <option value="all">Just give me all options!</option>
+              <option disabled value="">Deployment Region</option>
+              <option v-for="region in regions" :value="region.Slug">
+                {{ region.Name }}
+              </option>
             </select>
             <span class="input-group-addon">Location</span>
           </div>
@@ -238,7 +206,8 @@
         runtimepermonth: '',
         runtimeinmonths: '',
         result: [],
-        vmsizes: []
+        vmsizes: [],
+        regions: []
       }
     },
     computed: {
@@ -366,7 +335,7 @@
       }
     },
     methods: {
-      getMetaData() {
+      getMetaDataVmSizes() {
         var vmchooserurl = config.apiGetVmSizes
         var vmchooserconfig = {
           headers: {
@@ -376,8 +345,25 @@
         }
         axios.post(vmchooserurl, '', vmchooserconfig)
           .then(response => {
-            console.log(response.data)
+            // console.log(response.data)
             this.vmsizes = response.data
+          })
+          .catch(e => {
+            this.errors.push(e)
+          })
+      },
+      getMetaDataRegions() {
+        var vmchooserurl = config.apiGetRegions
+        var vmchooserconfig = {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Ocp-Apim-Subscription-Key': ''
+          }
+        }
+        axios.post(vmchooserurl, '', vmchooserconfig)
+          .then(response => {
+            // console.log(response.data)
+            this.regions = response.data
           })
           .catch(e => {
             this.errors.push(e)
@@ -434,7 +420,8 @@
       }
     },
     mounted: function () {
-      this.getMetaData()
+      this.getMetaDataRegions()
+      this.getMetaDataVmSizes()
     }
   }
 </script>
--- src/config/prod/index.js
+++ src/config/prod/index.js
@@ -2,6 +2,7 @@ export default {
   serverURI: '',
   apiGetVmSize: 'https://vmchooser.azure-api.net/v2/api/GetVmSize',
   apiGetVmSizes: 'https://vmchooser.azure-api.net/v2/api/GetVmSizes',
+  apiGetRegions: 'https://vmchooser.azure-api.net/dev-v2/api/GetRegions',
   apiGetVmDetail: 'https://vmchooser.azure-api.net/v2/api/GetVmDetail',
   apiCalcVmOptimizations: 'https://vmchooser.azure-api.net/v2/api/CalcVmOptimizations',
   apiGetDiskConfig: 'https://vmchooser.azure-api.net/v2/api/GetDiskConfig',
