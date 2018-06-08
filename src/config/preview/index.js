69e8612981d3af56082acb449d0319ac9c0ddeba
Karim Vaes <karim@kvaes.be>
2018-06-08 16:44:05 +0200

    Moved preview back to dev backend

--- src/config/preview/index.js
+++ src/config/preview/index.js
@@ -1,11 +1,11 @@
 export default {
   serverURI: '',
-  apiGetVmSize: 'https://vmchooser.azure-api.net/v2/api/GetVmSize',
-  apiGetVmSizes: 'https://vmchooser.azure-api.net/v2/api/GetVmSizes',
-  apiGetRegions: 'https://vmchooser.azure-api.net/v2/api/GetRegions',
-  apiGetVmDetail: 'https://vmchooser.azure-api.net/v2/api/GetVmDetail',
-  apiCalcVmOptimizations: 'https://vmchooser.azure-api.net/v2/api/CalcVmOptimizations',
-  apiGetDiskConfig: 'https://vmchooser.azure-api.net/v2/api/GetDiskConfig',
+  apiGetVmSize: 'https://vmchooser.azure-api.net/dev-v2/api/GetVmSize',
+  apiGetVmSizes: 'https://vmchooser.azure-api.net/dev-v2/api/GetVmSizes',
+  apiGetRegions: 'https://vmchooser.azure-api.net/dev-v2/api/GetRegions',
+  apiGetVmDetail: 'https://vmchooser.azure-api.net/dev-v2/api/GetVmDetail',
+  apiCalcVmOptimizations: 'https://vmchooser.azure-api.net/dev-v2/api/CalcVmOptimizations',
+  apiGetDiskConfig: 'https://vmchooser.azure-api.net/dev-v2/api/GetDiskConfig',
   fixedLayout: false,
   hideLogoOnMobile: false
 }
