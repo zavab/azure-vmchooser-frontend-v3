05fb5f0932cf224d4353ce72f632765ced2e0790
Karim Vaes <Karim.Vaes@microsoft.com>
2020-01-27 21:16:40 +0000

    Removed TM from deploy2cluster

--- helm/discoveranddeploy.sh
+++ helm/discoveranddeploy.sh
@@ -32,9 +32,9 @@ function deployToCluster {
   clusterrg=$(getClusterResourcegroup $clustername)
   getClusterKubectl $clustername $clusterrg
   lastversion=`helm history "$deploymentname" | tail -n 1 | awk '{ print $1 }'`
-  helm status "$deploymentname" | grep -i deployed && action="upgrade" || action="install"
-  helm $action "$deploymentname" "$helmregistry" --version "$version" -n "$namespace" -f "$valuesfile" || helm rollback "$deploymentname" "$lastversion"
-  addService2TrafficeManager $clustername
+  helm status "$deploymentname" | grep -i 'deployed' && action="upgrade" || action="install"
+  helm $action "$deploymentname" "$helmregistry" --version "$version" -n "$namespace" -f "$valuesfile" || helm rollback "$deploymentname" "$lastversion" || helm delete "$deploymentname"
+  # addService2TrafficeManager $clustername (commented out due to the usage of an ingress controller that will handle this endpoint)
 }
 
 function getTrafficManager {
